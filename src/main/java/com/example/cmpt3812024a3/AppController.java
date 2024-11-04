6bdc28b8f3e5a593ed1d0dc7f7dac22b72a36b41
Sayed Farzaan Rafi Bhat (kfn036) <kfn036@usask.ca>
2024-11-03 21:04:27 -0600

    Created the controller class that manages the interactions

---
+++ src/main/java/com/example/cmpt3812024a3/AppController.java
@@ -0,0 +1,101 @@
+package com.example.cmpt3812024a3;
+
+import javafx.scene.input.MouseEvent;
+
+public class AppController {
+
+    private EntityModel model;
+    private InteractionModel imodel;
+    private ControllerState currentState;
+    private double prevX, prevY, dx, dy, dw, dh;
+
+    /**
+     * Constructor for the Controller class
+     */
+    public AppController() {
+        currentState = ready;
+    }
+
+    /**
+     * Set the model
+     * @param m model
+     */
+    public void setModel(EntityModel m) { this.model = m; }
+
+    /**
+     * Set the imodel
+     * @param im imodel
+     */
+    public void setIModel(InteractionModel im) { this.imodel = im; }
+
+    /**
+     * Handle the Mouse Pressed event
+     * @param event mouse event
+     */
+    public void handlePressed(MouseEvent event) { currentState.handlePressed(event); }
+
+    /**
+     * Handle the Mouse Dragged event
+     * @param event mouse event
+     */
+    public void handleDragged(MouseEvent event) { currentState.handleDragged(event); }
+
+    /**
+     * Handle the Mouse Released event
+     * @param event mouse event
+     */
+    public void handleReleased(MouseEvent event) { currentState.handleReleased(event); }
+
+    /**
+     * Public class that defines the controller state & methods
+     */
+    public abstract static class ControllerState {
+        void handlePressed(MouseEvent event) { }
+        void handleDragged(MouseEvent event) { }
+        void handleReleased(MouseEvent event) { }
+    }
+
+    /**
+     * READY state
+     */
+    ControllerState ready = new ControllerState() {
+
+        public void handlePressed(MouseEvent event) { }
+    };
+
+    /**
+     * DRAGGING state
+     */
+    ControllerState dragging = new ControllerState() {
+
+        public void handleDragged(MouseEvent event) { }
+
+        public void handleReleased(MouseEvent event) { }
+
+    };
+
+    /**
+     * CREATE OR UNSELECT state
+     */
+    ControllerState create_or_unselect = new ControllerState() {
+
+        public void handleDragged(MouseEvent event) { }
+
+        public void handleReleased(MouseEvent event) { }
+
+    };
+
+    /**
+     * CREATING state
+     */
+    ControllerState creating = new ControllerState() {
+
+        public void handleDragged(MouseEvent event) { };
+
+        public void handleReleased(MouseEvent event) { }
+
+    };
+
+
+
+}
