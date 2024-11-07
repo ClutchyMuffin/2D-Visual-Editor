f48e7e642d82046a5e69472e10df03e62dd5454f
Sayed Farzaan Rafi Bhat (kfn036) <kfn036@usask.ca>
2024-11-07 02:58:05 -0600

    Updated InteractionModel - Holds the total worldSize of 2000x2000 - Has an arraylist of views as subscribers, and functions to add & notify them - moveViewPort function to move the view by the given values - Getters and Setters for the viewPort

--- src/main/java/com/example/cmpt3812024a3/InteractionModel.java
+++ src/main/java/com/example/cmpt3812024a3/InteractionModel.java
@@ -1,14 +1,20 @@
 package com.example.cmpt3812024a3;
 
+import java.util.ArrayList;
+
 public class InteractionModel {
 
+    private final ArrayList<Subscriber> subs;
     private Box selectedBox;
+    private double worldSize = 2000;
+    private double viewLeft, viewTop;
 
     /**
      * Constructor for the InteractionModel Class
      */
     public InteractionModel() {
         selectedBox = null;
+        subs = new ArrayList<>();
     }
 
     /**
@@ -27,5 +33,69 @@ public class InteractionModel {
         this.selectedBox = selectedBox;
     }
 
+    /**
+     * Returns the size of the world in pixels
+     * @return world size
+     */
+    public double getWorldSize() {
+        return worldSize;
+    }
+
+    /**
+     * Returns the left offset
+     * @return left
+     */
+    public double getViewLeft() {
+        return viewLeft;
+    }
+
+    /**
+     * Sets the left offset to the given value
+     * @param viewLeft value to set
+     */
+    public void setViewLeft(double viewLeft) {
+        this.viewLeft = viewLeft;
+    }
+
+    /**
+     * Returns the top offset
+     * @return top
+     */
+    public double getViewTop() {
+        return viewTop;
+    }
+
+    /**
+     * Sets the top offset to the given value
+     * @param viewTop value to set
+     */
+    public void setViewTop(double viewTop) {
+        this.viewTop = viewTop;
+    }
+
+    /**
+     * Move the viewport by the given values
+     * @param dx x-axis movement
+     * @param dy y-axis movement
+     */
+    public void moveViewPort(double dx, double dy) {
+        viewLeft += dx;
+        viewTop += dy;
+    }
+
+    /**
+     * Add a new subscriber to the model
+     * @param sub new sub
+     */
+    public void addSubscriber(Subscriber sub) {
+        subs.add(sub);
+    }
+
+    /**
+     * Notify the subscribers about the change in the model
+     */
+    public void notifySubscribers() {
+        subs.forEach(Subscriber::modelChanged);
+    }
 
 }
