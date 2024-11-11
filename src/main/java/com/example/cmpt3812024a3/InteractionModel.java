40d40be6b6cebee8b27e68733fa1ccd63b77ee2f
Sayed Farzaan Rafi Bhat (kfn036) <kfn036@usask.ca>
2024-11-11 00:51:29 -0600

    Added functions to return which handle of the selected Box is being clicked on - Also updated variable and function names to be more verbose

--- src/main/java/com/example/cmpt3812024a3/InteractionModel.java
+++ src/main/java/com/example/cmpt3812024a3/InteractionModel.java
@@ -8,12 +8,16 @@ public class InteractionModel {
     private Box selectedBox;
     private double worldSize = 2000;
     private double viewPortLeft, viewPortTop, viewPortWidth, viewPortHeight;
-    private double handleRadius = 4;
+    private double handleRadius = 30;
 
     /**
      * Constructor for the InteractionModel Class
      */
     public InteractionModel() {
+        viewPortLeft = 0;
+        viewPortTop = 0;
+        viewPortWidth = 800;
+        viewPortHeight = 800;
         selectedBox = null;
         subs = new ArrayList<>();
     }
@@ -27,40 +31,42 @@ public class InteractionModel {
      */
     public void moveViewPort(double dx, double dy) {
 
-        double newViewPortLeft = viewPortLeft + dx;
-        if (newViewPortLeft > 0) {
-            viewPortLeft = 0;
-        }
-        else viewPortLeft = Math.max(newViewPortLeft, -worldSize + viewPortWidth);
+        double newViewPortLeft, newViewPortTop;
 
-        double newViewPortTop = viewPortTop + dy;
-        if (newViewPortTop > 0) {
-            viewPortTop = 0;
+        newViewPortLeft = viewPortLeft + dx;
+        if (newViewPortLeft + viewPortWidth > worldSize) {
+            newViewPortLeft = worldSize - viewPortWidth;
+            setViewPortLeft(newViewPortLeft);
         }
-        else viewPortTop = Math.max(newViewPortTop, -worldSize + viewPortHeight);
+        else if (newViewPortLeft < 0) {
+            setViewPortLeft(0);
+        }
+
+        newViewPortTop = viewPortTop + dy;
+        if (newViewPortTop + viewPortHeight > worldSize) {
+            newViewPortTop = worldSize - viewPortHeight;
+            setViewPortTop(newViewPortTop);
+        }
+        else if (newViewPortTop < 0) {
+            setViewPortTop(0);
+        }
+
+        viewPortLeft += dx;
+        viewPortTop += dy;
         notifySubscribers();
     }
 
 
     // ----------------- HANDLES ----------------- //
 
-    public boolean onHandle(double mx, double my) {
-        if (topLeftHandle(mx, my)) {
-            System.out.println("topLeftHandle");
-        }
-        else if (topRightHandle(mx, my)) {
-            System.out.println("topRightHandle");
-        }
-        else if (bottomLeftHandle(mx, my)) {
-            System.out.println("bottomLeftHandle");
-        }
-        else if (bottomRightHandle(mx, my)) {
-            System.out.println("bottomRightHandle");
-        }
-        return selectedBox != null && (topLeftHandle(mx,my) || topRightHandle(mx,my) || bottomLeftHandle(mx,my) || bottomRightHandle(mx,my));
-    }
+    /**
+     * Return an integer to denote which handle is clicked
+     * @param mx x-coordinate
+     * @param my y-coordinate
+     * @return 1 = top-left, 2 = top-right, 3 = bottom-left, 4 = bottom-right
+     */
+    public int onHandle(double mx, double my) {
 
-    public int whichHandle(double mx, double my) {
         if (topLeftHandle(mx, my)) {
             return 1;
         }
@@ -76,32 +82,50 @@ public class InteractionModel {
         return 0;
     }
 
+    /**
+     * Check if the top-left handle is clicked
+     * @param mx x-coordinate
+     * @param my y-coordinate
+     * @return true if clicked, else false
+     */
     public boolean topLeftHandle(double mx, double my) {
-        double selX = selectedBox.getX();
-        double selY = selectedBox.getY();
-        return Math.hypot(selX - mx, selY - my) <= handleRadius;
+        return Math.hypot(selectedBox.getX() - mx,
+                          selectedBox.getY() - my) <= handleRadius;
     }
 
+    /**
+     * Check if the top-right handle is clicked
+     * @param mx x-coordinate
+     * @param my y-coordinate
+     * @return true if clicked, else false
+     */
     public boolean topRightHandle(double mx, double my) {
-        double selX = selectedBox.getX();
-        double selY = selectedBox.getY();
-        double selW = selectedBox.getW();
-        return Math.hypot(selX + selW - mx, selY - my) <= handleRadius;
+        return Math.hypot(selectedBox.getX() + selectedBox.getWidth() - mx,
+                          selectedBox.getY() - my) <= handleRadius;
+
     }
 
+    /**
+     * Check if the bottom-left handle is clicked
+     * @param mx x-coordinate
+     * @param my y-coordinate
+     * @return true if clicked, else false
+     */
     public boolean bottomLeftHandle(double mx, double my) {
-        double selX = selectedBox.getX();
-        double selY = selectedBox.getY();
-        double selH = selectedBox.getH();
-        return Math.hypot(selX - mx, selY + selH - my) <= handleRadius;
+        return Math.hypot(selectedBox.getX() - mx,
+                          selectedBox.getY() + selectedBox.getHeight() - my) <= handleRadius;
+
     }
 
+    /**
+     * Check if the bottom-right handle is clicked
+     * @param mx x-coordinate
+     * @param my y-coordinate
+     * @return true if clicked, else false
+     */
     public boolean bottomRightHandle(double mx, double my) {
-        double selX = selectedBox.getX();
-        double selY = selectedBox.getY();
-        double selW = selectedBox.getW();
-        double selH = selectedBox.getH();
-        return Math.hypot(selX + selW - mx, selY + selH - my) <= handleRadius;
+        return Math.hypot(selectedBox.getX() + selectedBox.getWidth() - mx,
+                          selectedBox.getY() + selectedBox.getHeight() - my) <= handleRadius;
     }
 
 
@@ -156,31 +180,46 @@ public class InteractionModel {
      * Set the given box as the selected box
      * @param selectedBox box to set as selected
      */
-    public void setSelectedBox(Box selectedBox) { this.selectedBox = selectedBox; }
+    public void setSelectedBox(Box selectedBox) {
+        this.selectedBox = selectedBox;
+        notifySubscribers();
+    }
 
     /**
      * Sets the width of the viewport to the given value
      * @param newViewWidth value to set
      */
-    public void setViewPortWidth(double newViewWidth) { this.viewPortWidth = newViewWidth; }
+    public void setViewPortWidth(double newViewWidth) {
+        this.viewPortWidth = newViewWidth;
+        notifySubscribers();
+    }
 
     /**
      * Sets the height of the viewport to the given value
      * @param newViewHeight value to set
      */
-    public void setViewPortHeight(double newViewHeight) { this.viewPortHeight = newViewHeight; }
+    public void setViewPortHeight(double newViewHeight) {
+        this.viewPortHeight = newViewHeight;
+        notifySubscribers();
+    }
 
     /**
      * Sets the left offset of the viewport to the given value
      * @param newViewLeft value to set
      */
-    public void setViewPortLeft(double newViewLeft) { this.viewPortLeft = newViewLeft; }
+    public void setViewPortLeft(double newViewLeft) {
+        this.viewPortLeft = newViewLeft;
+        notifySubscribers();
+    }
 
     /**
      * Sets the top offset of the viewport to the given value
      * @param newViewTop value to set
      */
-    public void setViewPortTop(double newViewTop) { this.viewPortTop = newViewTop; }
+    public void setViewPortTop(double newViewTop) {
+        this.viewPortTop = newViewTop;
+        notifySubscribers();
+    }
 
 
     // ----------------- SUBSCRIBERS ----------------- //
