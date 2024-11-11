baf557f8ede5ed1ef009f5f4f7fdb90d986609f2
Sayed Farzaan Rafi Bhat (kfn036) <kfn036@usask.ca>
2024-11-11 17:22:21 -0600

    Overriding the functions of Box in Portal to also update the portal viewPort positions

--- src/main/java/com/example/cmpt3812024a3/Portal.java
+++ src/main/java/com/example/cmpt3812024a3/Portal.java
@@ -2,22 +2,42 @@ package com.example.cmpt3812024a3;
 
 public class Portal extends Box {
     private double scaleFactor;
-    private double viewPortLeft, viewPortTop;
+    private double portalLeft, portalTop;
 
     public Portal(double x, double y, double width, double height) {
         super(x, y, width, height);
         this.scaleFactor = 0.5;
-        this.viewPortLeft = 0;
-        this.viewPortTop = 0;
+        this.portalLeft = 0;
+        this.portalTop = 0;
     }
 
+    // ----------------- ADDERS ----------------- //
+    public void addX(double dx) {
+        super.addX(dx);
+        this.portalLeft += dx;
+    }
+    public void addY(double dy) {
+        super.addY(dy);
+        this.portalTop += dy;
+    }
+
+    // ----------------- GETTERS ----------------- //
     public double getScaleFactor() { return scaleFactor; }
+    public double getPortalLeft() { return portalLeft; }
+    public double getPortalTop() { return portalTop; }
+
+    // ----------------- SETTERS ----------------- //
     public void setScaleFactor(double scaleFactor) { this.scaleFactor = scaleFactor; }
+    public void setPortalLeft(double portalLeft) { this.portalLeft = portalLeft; }
+    public void setPortalTop(double portalTop) { this.portalTop = portalTop; }
 
-    public double getViewPortLeft() { return viewPortLeft; }
-    public void setViewPortLeft(double viewPortLeft) { this.viewPortLeft = viewPortLeft; }
-
-    public double getViewPortTop() { return viewPortTop; }
-    public void setViewPortTop(double viewPortTop) { this.viewPortTop = viewPortTop; }
+    public void setX(double nx) {
+        super.setX(nx);
+        this.portalLeft = nx;
+    }
+    public void setY(double ny) {
+        super.setY(ny);
+        this.portalTop = ny;
+    }
 
 }
