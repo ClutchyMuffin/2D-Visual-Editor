bd88fc2d83cbe8704cf1a76e681dd3c3b629e3de
Sayed Farzaan Rafi Bhat (kfn036) <kfn036@usask.ca>
2024-11-10 23:43:24 -0600

    Fixed the EditorApp class launch

--- src/main/java/com/example/cmpt3812024a3/EditorApp.java
+++ src/main/java/com/example/cmpt3812024a3/EditorApp.java
@@ -5,14 +5,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class EditorApp extends Application {
+
     @Override
     public void start(Stage stage) throws Exception {
         MainUI mainUI = new MainUI();
         Scene scene = new Scene(mainUI);
 
-        stage.setScene(scene);
-        stage.setResizable(true);
         stage.setTitle("Editor App");
+        stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
